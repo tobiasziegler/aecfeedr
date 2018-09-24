20adbdf1180c3ef7efcb40791242768fef268d0f
David Mallard <correspondence@davidmallard.id.au>
2018-09-24 10:56:52 +1000

    Cache the extracted namespaces for use in XPath searches

--- R/results.R
+++ R/results.R
@@ -10,18 +10,26 @@ read_results_house_fp_by_pollingplace <- function(x) {
   # Read the XML document
   xml <- xml2::read_xml(x)
 
+  # Cache the extracted namespaces for use in XPath searches
+  ns <- xml2::xml_ns(xml)
+
   # Start by finding the nodeset containing each House contest in the document
-  contests <- xml2::xml_find_all(xml, "/d1:MediaFeed/d1:Results/d1:Election/d1:House/d1:Contests/d1:Contest")
+  contests <- xml2::xml_find_all(
+    xml,
+    "/d1:MediaFeed/d1:Results/d1:Election/d1:House/d1:Contests/d1:Contest",
+    ns = ns
+  )
 
   # Create a tibble by parsing the relevant XML nodes
   results <- tibble::tibble(
     contest_id = contests %>%
-      xml2::xml_find_first("./eml:ContestIdentifier") %>%
+      xml2::xml_find_first("./eml:ContestIdentifier", ns = ns) %>%
       xml2::xml_attr("Id"),
     pp_xml = contests %>%
       purrr::map(~ xml2::xml_find_all(
         .,
-        "./d1:PollingPlaces/d1:PollingPlace"
+        "./d1:PollingPlaces/d1:PollingPlace",
+        ns = ns
       ))
   )
 
@@ -32,12 +40,13 @@ read_results_house_fp_by_pollingplace <- function(x) {
       pollingplaces = pp_xml %>%
         purrr::map(~ tibble::tibble(
           pollingplace_id = .x %>%
-            xml2::xml_find_first("./d1:PollingPlaceIdentifier") %>%
+            xml2::xml_find_first("./d1:PollingPlaceIdentifier", ns = ns) %>%
             xml2::xml_attr("Id"),
           fp_xml = .x %>%
             purrr::map(~ xml2::xml_find_all(
               .,
-              "./d1:FirstPreferences/*"
+              "./d1:FirstPreferences/*",
+              ns = ns
             ))
         ))
     )
@@ -61,10 +70,10 @@ read_results_house_fp_by_pollingplace <- function(x) {
           vote_type = .x %>%
             xml2::xml_name(),
           candidate_id = .x %>%
-            xml2::xml_find_first("./eml:CandidateIdentifier") %>%
+            xml2::xml_find_first("./eml:CandidateIdentifier", ns = ns) %>%
             xml2::xml_attr("Id"),
           votes = .x %>%
-            xml2::xml_find_first("./d1:Votes") %>%
+            xml2::xml_find_first("./d1:Votes", ns = ns) %>%
             xml2::xml_text()
         ))
     )
