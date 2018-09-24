a0b2d79a669fbd14876ce451baa16e82d1cbbb83
David Mallard <correspondence@davidmallard.id.au>
2018-09-24 10:29:22 +1000

    Make the XPath searches match the XML structure more specifically

--- R/results.R
+++ R/results.R
@@ -11,17 +11,17 @@ read_results_house_fp_by_pollingplace <- function(x) {
   xml <- xml2::read_xml(x)
 
   # Start by finding the nodeset containing each House contest in the document
-  contests <- xml2::xml_find_all(xml, "//d1:House/d1:Contests/d1:Contest")
+  contests <- xml2::xml_find_all(xml, "/d1:MediaFeed/d1:Results/d1:Election/d1:House/d1:Contests/d1:Contest")
 
   # Create a tibble by parsing the relevant XML nodes
   results <- tibble::tibble(
     contest_id = contests %>%
-      xml2::xml_find_first(".//eml:ContestIdentifier") %>%
+      xml2::xml_find_first("./eml:ContestIdentifier") %>%
       xml2::xml_attr("Id"),
     pp_xml = contests %>%
       purrr::map(~ xml2::xml_find_all(
         .,
-        ".//d1:PollingPlaces/d1:PollingPlace"
+        "./d1:PollingPlaces/d1:PollingPlace"
       ))
   )
 
@@ -32,7 +32,7 @@ read_results_house_fp_by_pollingplace <- function(x) {
       pollingplaces = pp_xml %>%
         purrr::map(~ tibble::tibble(
           pollingplace_id = .x %>%
-            xml2::xml_find_first(".//d1:PollingPlaceIdentifier") %>%
+            xml2::xml_find_first("./d1:PollingPlaceIdentifier") %>%
             xml2::xml_attr("Id"),
           fp_xml = .x %>%
             purrr::map(~ xml2::xml_find_all(
