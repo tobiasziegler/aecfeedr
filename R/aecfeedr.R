4c7d0b9138be87b6aec2f0cb3f7df96c18b4c742
David Mallard <correspondence@davidmallard.id.au>
2020-06-27 13:33:55 +1000

    Import all rlang features, e.g. embracing

--- R/aecfeedr.R
+++ R/aecfeedr.R
@@ -1,6 +1,6 @@
 #' @keywords internal
 #'
-#' @importFrom rlang .data "!!!"
+#' @import rlang
 #' @importFrom xml2 read_xml xml_ns xml_find_first xml_find_all
 #'   xml_name xml_attr xml_text
 "_PACKAGE"
